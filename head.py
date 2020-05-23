b94c10def6f8c958db5ba1afa49c54151c663318
r07922097 <r07922097@ntu.edu.tw>
2020-05-23 21:14:46 +0800

    Init nn.Linear weights

--- head.py
+++ head.py
@@ -1,6 +1,14 @@
 import torch
 import torch.nn as nn
 
+def weights_init_uniform(m):
+    classname = m.__class__.__name__
+    # for every Linear layer in a model..
+    if classname.find('Linear') != -1:
+        # apply a uniform distribution to the weights and a bias=0
+        m.weight.data.uniform_(0.0, 1.0)
+        m.bias.data.fill_(0)
+
 class My_linear(nn.Module):
     def __init__(self, hidden_dim=768):
         super(My_linear, self).__init__()
@@ -9,6 +17,9 @@ class My_linear(nn.Module):
         self.actv = nn.LeakyReLU(0.1)
         self.norm = nn.LayerNorm(self.hidden_dim)
         self.linear_2 = nn.Linear(self.hidden_dim, 1)
+
+        weights_init_uniform(self.linear_1)
+        weights_init_uniform(self.linear_2)
 
 
     def forward(self,x): # x of shape (batch_size, hidden_dim)
